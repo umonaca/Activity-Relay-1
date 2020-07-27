fa7eb554ead91e737f64c311b33737764ae89798
Naoki Kosaka <naoki_kosaka@yukimochi.jp>
2020-07-27 10:11:07 +0900

    Add tests for models module.

--- models/state_test.go
+++ models/state_test.go
@@ -121,6 +121,25 @@ func createDemoSubscription() subscription {
 		Available:  true,
 	}
 }
+
+func TestRelayState_GetDomainConfig(t *testing.T) {
+	t.Run("return error for unknown option label", func(t *testing.T) {
+		relayState := createRelayState(t, nil)
+
+		refreshListener := make(chan bool)
+		err := relayState.StateRefreshListener(refreshListener)
+		if err != nil {
+			t.Error(err)
+		}
+
+		_, err = relayState.GetDomainConfig(12345)
+		if err == nil {
+			<-refreshListener
+			t.Error("Failed return error, given unknown option label")
+		}
+	})
+}
+
 func TestRelayState_SetDomainConfig(t *testing.T) {
 	t.Run("success set and get limited and blacked domain", func(t *testing.T) {
 		var labels = []DomainConfigTypeIota{
@@ -165,6 +184,125 @@ func TestRelayState_SetDomainConfig(t *testing.T) {
 			} else {
 				<-refreshListener
 			}
+		}
+	})
+
+	t.Run("return error for unknown option label", func(t *testing.T) {
+		relayState := createRelayState(t, nil)
+
+		refreshListener := make(chan bool)
+		err := relayState.StateRefreshListener(refreshListener)
+		if err != nil {
+			t.Error(err)
+		}
+
+		err = relayState.SetDomainConfig(12345, "example.yukimochi.dev")
+		if err == nil {
+			<-refreshListener
+			t.Error("Failed return error, given unknown option label")
+		}
+	})
+}
+
+func TestRelayState_DelDomainConfig(t *testing.T) {
+	t.Run("success del limited and blacked domain", func(t *testing.T) {
+		var labels = []DomainConfigTypeIota{
+			Limited,
+			Blocked,
+		}
+		relayState := createRelayState(t, nil)
+
+		refreshListener := make(chan bool)
+		err := relayState.StateRefreshListener(refreshListener)
+		if err != nil {
+			t.Error(err)
+		}
+
+		for _, label := range labels {
+			err := relayState.SetDomainConfig(label, "example.yukimochi.dev")
+			if err != nil {
+				t.Error(err)
+			} else {
+				<-refreshListener
+			}
+
+			err = relayState.DelDomainConfig(label, "example.yukimochi.dev")
+			if err != nil {
+				t.Error(err)
+			} else {
+				<-refreshListener
+			}
+
+			value, err := relayState.GetDomainConfig(label)
+			if err != nil {
+				t.Error(err)
+			}
+
+			contain := false
+			for _, domain := range value {
+				if domain == "example.yukimochi.dev" {
+					contain = true
+				}
+			}
+
+			if contain != false {
+				t.Error("Failed del limited, blacked domain: " + strconv.Itoa(int(label)))
+			}
+		}
+	})
+
+	t.Run("return error for unknown domain", func(t *testing.T) {
+		var labels = []DomainConfigTypeIota{
+			Limited,
+			Blocked,
+		}
+		relayState := createRelayState(t, nil)
+
+		refreshListener := make(chan bool)
+		err := relayState.StateRefreshListener(refreshListener)
+		if err != nil {
+			t.Error(err)
+		}
+
+		for _, label := range labels {
+			err = relayState.DelDomainConfig(label, "example.yukimochi.dev")
+			if err == nil {
+				<-refreshListener
+				t.Error("Failed return error, given unknown domain")
+			}
+		}
+	})
+
+	t.Run("return error for unknown option label", func(t *testing.T) {
+		relayState := createRelayState(t, nil)
+
+		refreshListener := make(chan bool)
+		err := relayState.StateRefreshListener(refreshListener)
+		if err != nil {
+			t.Error(err)
+		}
+
+		err = relayState.DelDomainConfig(12345, "example.yukimochi.dev")
+		if err == nil {
+			<-refreshListener
+			t.Error("Failed return error, given unknown option label")
+		}
+	})
+}
+
+func TestRelayState_GetSubscriptions(t *testing.T) {
+	t.Run("return error for unknown SubscriptionType", func(t *testing.T) {
+		relayState := createRelayState(t, nil)
+
+		refreshListener := make(chan bool)
+		err := relayState.StateRefreshListener(refreshListener)
+		if err != nil {
+			t.Error(err)
+		}
+
+		_, err = relayState.GetSubscriptions(12345)
+		if err == nil {
+			t.Error("Failed return error, given unknown SubscriptionType")
 		}
 	})
 }
@@ -218,6 +356,75 @@ func TestRelayState_SetSubscription(t *testing.T) {
 			} else {
 				<-refreshListener
 			}
+		}
+	})
+}
+
+func TestRelayState_DelSubscription(t *testing.T) {
+	t.Run("success del subscribe, pending", func(t *testing.T) {
+		var labels = []SubscriptionTypeIota{
+			Subscribe,
+			Pending,
+		}
+		relayState := createRelayState(t, nil)
+
+		refreshListener := make(chan bool)
+		err := relayState.StateRefreshListener(refreshListener)
+		if err != nil {
+			t.Error(err)
+		}
+
+		for _, label := range labels {
+			srcSubscription := createDemoSubscription()
+			if label == Pending {
+				srcSubscription.Available = false
+			}
+
+			err := relayState.SetSubscription(srcSubscription)
+			if err != nil {
+				t.Error(err)
+			} else {
+				<-refreshListener
+			}
+
+			err = relayState.DelSubscription("example.yukimochi.dev")
+			if err != nil {
+				t.Error(err)
+			} else {
+				<-refreshListener
+			}
+
+			value, err := relayState.GetSubscriptions(label)
+			if err != nil {
+				t.Error(err)
+			}
+
+			contain := false
+			for _, subscription := range value {
+				if subscription.Domain == srcSubscription.Domain {
+					contain = true
+				}
+			}
+
+			if contain != false {
+				t.Error("Failed del subscribe, pending: " + strconv.Itoa(int(label)))
+			}
+		}
+	})
+
+	t.Run("return error for unknown domain", func(t *testing.T) {
+		relayState := createRelayState(t, nil)
+
+		refreshListener := make(chan bool)
+		err := relayState.StateRefreshListener(refreshListener)
+		if err != nil {
+			t.Error(err)
+		}
+
+		err = relayState.DelSubscription("example.yukimochi.dev")
+		if err == nil {
+			<-refreshListener
+			t.Error("Failed return error, given unknown option label")
 		}
 	})
 }
@@ -283,6 +490,22 @@ func TestRelayState_PromoteSubscription(t *testing.T) {
 			if contain != true {
 				t.Error("Failed promote pending to subscribe")
 			}
+		}
+	})
+
+	t.Run("return error for unknown domain", func(t *testing.T) {
+		relayState := createRelayState(t, nil)
+
+		refreshListener := make(chan bool)
+		err := relayState.StateRefreshListener(refreshListener)
+		if err != nil {
+			t.Error(err)
+		}
+
+		err = relayState.PromoteSubscription("example.yukimochi.dev")
+		if err == nil {
+			<-refreshListener
+			t.Error("Failed return error, given unknown domain")
 		}
 	})
 }
